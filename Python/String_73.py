3ef795ba91b962d9b0aebdabd7f95f55fb82db82
sravanthi.kotha <sravanthi.kotha.consultant@nielsen.com>
2017-08-09 15:34:55 +0530

    first push

---
+++ Python/String_73.py
@@ -0,0 +1,7 @@
+a = "I am a simple string \n"
+
+print ("length of my string is :"+str(len(a)))
+
+print( a * 5)
+
+input("press any key to exit.. ")
---
+++ Python/loop.py
@@ -0,0 +1,11 @@
+print ("Given a number to print the sequence of numbers")
+
+print(" Enter \'0\' to exist... ")
+
+b = int(input("Number plz!!"))
+
+a=1
+while a<=b and a!=0:
+    print(a)
+    a+=1
+    
